4aad4c870856371236a5c82db4e37d08259c3a75
ally804 <75229084+ally804@users.noreply.github.com>
2021-04-07 17:50:08 -0500

    edits to player class, implemented GameElement

--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,42 @@
 
-public class Player {
+public class Player implements GameElement{
+	
 	
 	int[] position = new int[2];
-	private int hp; //health points
-	private int bd; //base damage
-	private int STR; //strength
-	private int INT; //intelligence
-	private int CON; //constitution
-	private int SPD; //speed
+	public int hp; //health points
+	public int bd; //base damage
+	public int STR; //strength
+	public int INT; //intelligence
+	public int CON; //constitution
+	public int SPD; //speed
+	public int food;
+	public int smPot; //small potion
+	public int lgPot; //large potion
+	
 
 	
 	public Player() {
 		
-	this.position[0] =1;
-	this.position[1] =1;
-	this.hp = 100;
-	this.bd = 10;
-	this.STR = 10;
-	this.INT = 10;
-	this.CON = 10;
-	this.SPD = 10;
+		this.position[0] =1;
+		this.position[1] =1;
+		this.hp = 100;
+		this.bd = 10;
+		this.STR = 10;
+		this.INT = 10;
+		this.CON = 10;
+		this.SPD = 10;
+		this.food = 0;
+		this.smPot = 0;
+		this.lgPot = 0;
 	
+	}
+
+
+
+	@Override
+	public void write() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
